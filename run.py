228259cba3869f7ebbea4ef096957f4904114036
Wouter van den Dries <woutervddries@live.nl>
2023-08-16 19:16:50 +0000

    Add typewriter function to create a typewriter effect for all text loaded on screen. Add load_game function to display loading text. Add main function for game operation

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import sys,time,os
+
+# Typewriter function is heavily derived from Learn Learn Scratch Tutorials YouTube Channel
+# https://www.youtube.com/watch?v=2h8e0tXHfk0&t=135s
+
+def typewriter(text):
+    """
+    Creates a retro typewriter effect for text being loaded on the screen.
+    """
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+
+        if char != "\n":
+            time.sleep(0.04)
+        else:
+            time.sleep(1)
+
+def load_game():
+    typewriter("SITUATION CODE RED. I REPEAT. CODE RED.\n")
+    typewriter("Ah, there you are. Greetings admiral, we were expecting you.\n")
+    typewriter("The battle room is being prepared as we speak and our fleet is ready for deployment.\n")
+    typewriter("Bring us victory...\n")
+
+def main():
+    """
+    Run all program functions
+    """
+    load_game()
+
+main()
