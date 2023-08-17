dd72de0d655f12f5db7b188385c382e8ef556375
Wouter van den Dries <woutervddries@live.nl>
2023-08-17 09:27:43 +0000

    Change loading text. Add main menu banner in art.py and import it to run

--- run.py
+++ run.py
@@ -1,10 +1,8 @@
 import sys,time,os
-from art import boat_logo, text_logo
+from art import boat_logo, text_logo, menu_banner
 
-# Typewriter function is heavily derived from Learn Learn Scratch Tutorials YouTube Channel
+# Typewriter function is heavily derived from "Learn Learn Scratch Tutorials" YouTube Channel
 # https://www.youtube.com/watch?v=2h8e0tXHfk0&t=135s
-
-
 def typewriter(text):
     """
     Creates a retro typewriter effect for text being loaded on the screen.
@@ -20,17 +18,23 @@ def typewriter(text):
 
 def load_game():
     typewriter("Ah, there you are. Greetings admiral, we were expecting you.\n")
-    typewriter("The battle room is being prepared as we speak and our fleet is ready for deployment.\n")
-    typewriter("Bring us victory...\n")
+    typewriter("The battle room is operational and our fleet is ready for deployment.\n")
+    typewriter("Good luck admiral...\n")
+
+def load_main_menu():
+    os.system('clear')
+    print(boat_logo)
+    print(text_logo)
+    print(menu_banner)
+
 
 def main():
     """
     Run all program functions
     """
-    load_game()
-    os.system('clear')
-    print(boat_logo)
-    print(text_logo)
+    # load_game()
+    load_main_menu()
+
 
 main()
 
