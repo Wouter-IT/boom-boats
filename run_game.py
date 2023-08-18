5aeec6cfce0e8ecab9d6b196f9b58034b414dabc
Wouter van den Dries <woutervddries@live.nl>
2023-08-18 21:26:07 +0000

    Rework username_input verification to work without while loop

--- run_game.py
+++ run_game.py
@@ -3,17 +3,18 @@ import os
 from art import LOGO_TEXT, UNAME_BANNER
 from settings import user
 
+def create_brd():
+    brd = []
+
 def uname_registration():
     os.system('clear')
     print(LOGO_TEXT)
     print(UNAME_BANNER)
-    while True:
-        username_input = input("\nAdmiral, please choose a username. It has to be at least 1 character long:\n")
-        if username_input != "":
-            return False
-        else:
-            os.system('clear')
+    username_input = input("\nAdmiral, please choose a username. It has to be at least 1 character long:\n")
+    if username_input != "":
+        user['username'] = username_input
+    else:
+        os.system('clear')
         print(LOGO_TEXT)
         print(UNAME_BANNER)
         print("\nThe username cannot be empty.")
-    user['username'] = username_input
