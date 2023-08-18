9cbfa4301fa52fdedd977f5afa96e8150a35ca0f
Wouter van den Dries <woutervddries@live.nl>
2023-08-18 22:16:52 +0000

    Add create_brd function to setup board borders and logo on top.

--- run_game.py
+++ run_game.py
@@ -4,7 +4,18 @@ from art import LOGO_TEXT, UNAME_BANNER
 from settings import user
 
 def create_brd():
-    brd = []
+    os.system('clear')
+    print(LOGO_TEXT)
+    numerals_top = f"    1  2  3  4  5  6  7  "
+    border_top_bottom = f"  +---------------------+"
+    board
+    print(numerals_top)
+    print(border_top_bottom)
+    print(f)
+    print(border_top_bottom)
+    something = input('type something')
+
+
 
 def uname_registration():
     os.system('clear')
@@ -13,6 +24,7 @@ def uname_registration():
     username_input = input("\nAdmiral, please choose a username. It has to be at least 1 character long:\n")
     if username_input != "":
         user['username'] = username_input
+        create_brd()
     else:
         os.system('clear')
         print(LOGO_TEXT)
